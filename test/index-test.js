48d3a5d344ef613fea879c8a3cfc14383ab05d58
Joanne Basa <joannegbasa@gmail.com>
2017-10-09 12:25:43 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,7 +3,7 @@ var a, b
 beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
-  n = Math.floor(Math.random() * 1000)
+  n = Math.random() * 1000)
 })
 
 it('add(a, b) adds two numbers and returns the result', function() {
